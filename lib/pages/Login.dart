ecb930c66ad834959d01ea219fccec1a4682dfd3
Nelson_Olanipekun <olanipekunnelson@gmail.com>
2020-07-31 16:59:45 +0100

    complete sign in

--- lib/pages/Login.dart
+++ lib/pages/Login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:legal_precedents/pages/ForgotPassword.dart';
 
 class SignIn extends StatefulWidget {
   @override
@@ -6,7 +7,13 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
+  bool _showPassword = true;
+
   @override
+  submit() {
+    print("I have submitted");
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
@@ -44,14 +51,88 @@ class _SignInState extends State<SignIn> {
               ),
               TextField(
                 style: TextStyle(fontSize: 22),
+                obscureText: this._showPassword,
                 decoration: InputDecoration(
                   prefixIcon: Icon(Icons.lock),
+                  suffixIcon: IconButton(
+                      icon: Icon(this._showPassword
+                          ? Icons.visibility_off
+                          : Icons.visibility),
+                      onPressed: () {
+                        setState(() {
+                          this._showPassword = !this._showPassword;
+                        });
+                      }),
                   hintText: "Password",
                   contentPadding: EdgeInsets.only(left: 20, top: 20),
                   border: OutlineInputBorder(
                       borderRadius: BorderRadius.all(Radius.circular(10))),
                 ),
               ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.6,
+                child: RaisedButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30)),
+                  color: Theme.of(context).primaryColor,
+                  textColor: Colors.white,
+                  onPressed: () {},
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 10, bottom: 10),
+                    child: Text(
+                      "Sign In",
+                      style: TextStyle(fontSize: 24),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  GestureDetector(
+                    child: Text(
+                      "Forgot Password ?",
+                      style: TextStyle(
+                          color: Theme.of(context).primaryColor, fontSize: 18),
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => ForgotPassword()));
+                    },
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    "Dont have an account yet ? ",
+                    style: TextStyle(fontSize: 15),
+                  ),
+                  GestureDetector(
+                    child: Text(
+                      "Sign Up",
+                      style: TextStyle(
+                          color: Theme.of(context).primaryColor, fontSize: 18),
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => ForgotPassword()));
+                    },
+                  ),
+                ],
+              )
             ],
           ),
         ),
