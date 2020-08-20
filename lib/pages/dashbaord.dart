341a493d7b8c77d0afe79c3d4b4ad21a9cb87b32
Nelson_Olanipekun <olanipekunnelson@gmail.com>
2020-08-20 16:38:37 +0100

    Add Quote works

--- lib/pages/dashbaord.dart
+++ lib/pages/dashbaord.dart
@@ -12,81 +12,82 @@ class _DashbaordState extends State<Dashbaord> {
 
   Future<String> createAlertDialog(BuildContext context) {
     final maxLines = 5;
-    final _formkey = GlobalKey <FormState>();
+    final _formKey = GlobalKey<FormState>();
     TextEditingController wordsController = new TextEditingController();
     TextEditingController authorController = new TextEditingController();
 
     return showDialog(
         context: context,
-    builder: (context){
-      return AlertDialog(
-        elevation: 10,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(10.0))
-      ),
-        title: Text("Add Quote"),
-        content: Container(
-          height: MediaQuery.of(context).size.height * 0.3,
-          child: Form(
-            key: _formkey,
-            child: Column(
-              children: <Widget> [
-                Container(
-                  height: maxLines.toDouble() * 30,
-                  child: TextFormField(
-                    validator: (value) {
-                      if (value.isEmpty){
-                        return 'This field cannot be left blank';
-                      }
-                      return null;
-                    },
-                    maxLines: maxLines,
-                    controller: authorController,
-                    decoration: InputDecoration(
-                      hintText: "Write your Law Quote",
-                      fillColor: Theme.of(context).primaryColorLight,
-                      filled: true),
-                    ),
+        builder: (context) {
+          return AlertDialog(
+            elevation: 10,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10.0))),
+            title: Text("Add Quote"),
+            content: Container(
+              height: MediaQuery.of(context).size.height * 0.3,
+              child: SingleChildScrollView(
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        height: maxLines.toDouble() * 30,
+                        child: TextFormField(
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'This field cannot be left blank';
+                            }
+                            return null;
+                          },
+                          maxLines: maxLines,
+                          controller: authorController,
+                          decoration: InputDecoration(
+                              hintText: "Write your Law Quote",
+                              fillColor: Theme.of(context).primaryColorLight,
+                              filled: true),
+                        ),
+                      ),
+                      TextFormField(
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return 'This field cannot be left blank';
+                          }
+                          return null;
+                        },
+                        controller: wordsController,
+                        decoration: InputDecoration(
+                            hintText: "Author",
+                            fillColor: Theme.of(context).primaryColorLight,
+                            filled: true),
+                      ),
+                    ],
                   ),
-                TextFormField(
-                  validator: (value){
-                    if (value.isEmpty){
-      return 'This field cannot be left blank';
-      }
-                    return null;
-                    },
-      controller: wordsController,
-      decoration: InputDecoration(
-        hintText: 'Author',
-        fillColor: Theme.of(context).primaryColorLight,
-        filled: true),
-      ),
                 ),
-                 ],
+              ),
             ),
-          ),
-        ),
-      actions: <Widget> [
-        MaterialButton(
-      elevation: 5.0,
-        child: Text('submit'),
-      onPressed: () {
-    if(_formkey.currentState.validate()) {
-    Navigator.of(context).pop();
-      }
-      },
-      ),
-      MaterialButton(
-      elevation: 5.0,
-      child: Text('Cancel'),
-      onPressed: () {
-        Navigator.of(context).pop();
-      },
-      ),
-      ],
-      ),
-    }),
+            actions: <Widget>[
+              MaterialButton(
+                elevation: 5.0,
+                child: Text("Submit"),
+                onPressed: () {
+                  if (_formKey.currentState.validate()) {
+                    Navigator.of(context).pop();
+                  }
+                },
+              ),
+              MaterialButton(
+                elevation: 5.0,
+                child: Text("Cancel"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -109,7 +110,9 @@ class _DashbaordState extends State<Dashbaord> {
         ),
       ),
       floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {},
+        onPressed: () async {
+          await createAlertDialog(context);
+        },
         label: Text(
           "Create Post",
           style: TextStyle(fontSize: 20),
